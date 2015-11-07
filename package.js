d9a7fe82b1390fa6f60feb4b0e55c1ef19671114
Gabriel Rubens <grsabreu@gmail.com>
2015-11-07 15:56:37 -0200

    Update Package infos and syntax

--- package.js
+++ package.js
@@ -1,36 +1,58 @@
-Npm.depends({
-  "handlebars": "1.0.10"
-});
-
 Package.describe({
-  summary: "Allows handlebars templates to be defined on the server in .handlebars files",
-  version: "0.2.0"
+  name: 'cmather:handlebars-server',
+  version: '1.0.0',
+  summary: 'Allows handlebars templates to be defined on the server in .handlebars files',
+  git: 'https://github.com/cmather/meteor-handlebars-server',
+  documentation: 'README.md',
 });
 
-Package._transitional_registerBuildPlugin({
-  name: "compileServerHandlebarsTemplates",
-  use: ["handlebars"],
+Npm.depends({
+  'handlebars': '4.0.4',
+});
+
+Package.registerBuildPlugin({
+  name: 'compileServerHandlebarsTemplates',
+
+  use: [
+    'caching-compiler@1.0.0',
+    'ecmascript',
+    'ejson',
+  ],
+
   sources: [
-    'plugin/compile-handlebars.js'
-  ]
+    'plugin/compile-handlebars.js',
+  ],
+
+  npmDependencies: {
+    'handlebars': '4.0.4',
+  },
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR-CORE@0.9.0-rc12');
-  api.use(['handlebars', 'underscore'], 'server');
-  api.add_files('handlebars-server.js', 'server');
-  api.export('Handlebars', 'server');
-  api.export('OriginalHandlebars', 'server');
+Package.onUse(function(api){
+  api.versionsFrom('1.2.0.1');
+
+  api.use([
+    'ecmascript',
+    'underscore',
+    'isobuild:compiler-plugin@1.0.0',
+  ], 'server');
+
+  api.addFiles('handlebars-server.js', 'server');
+
+  api.export([
+    'Handlebars',
+    'OriginalHandlebars',
+  ], 'server');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use([
    'cmather:handlebars-server',
    'tinytest',
    'test-helpers'
   ], 'server');
 
-  api.add_files([
+  api.addFiles([
     'handlebars-server-tests.handlebars',
     'handlebars-server-tests-2.handlebars',
     'handlebars-server-tests.js'
