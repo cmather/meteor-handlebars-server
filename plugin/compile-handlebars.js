f23bbe62bcc4a5f58b310685d48f768ac937f933
Gabriel Rubens <grsabreu@gmail.com>
2015-11-07 15:56:56 -0200

    Update compile process

--- plugin/compile-handlebars.js
+++ plugin/compile-handlebars.js
@@ -1,37 +1,48 @@
-var path = Npm.require('path');
+/* jshint esnext: true */
+const path = Plugin.path;
 
-Plugin.registerSourceHandler("handlebars", function (compileStep) {
-  var contents = compileStep.read().toString('utf8')
-    , js;
+Plugin.registerCompiler({
+  extensions: ['handlebars', 'hbs'],
+  archMatching: 'os',
+}, () => new HandlebarsServer());
 
-  var path_part = path.dirname(compileStep.inputPath);
-  if (path_part === '.')
-    path_part = '';
+class HandlebarsServer extends CachingCompiler {
+  constructor(){
+    super({
+      compilerName: 'handlebars',
+      defaultCacheSize: 1024*1024*10,
+    });
+  }
 
-  if (path_part.length && path_part !== path.sep)
-    path_part = path_part + path.sep;
+  compileOneFile(file){
+    const output = this.compileHandlebar(file);
 
-  var ext = path.extname(compileStep.inputPath);
-  var basename = path.basename(compileStep.inputPath, ext);
-  var templateName = path.basename(compileStep.inputPath).match(/(.*)\.handlebars$/)[1];
-  
-  js = [
-    "Handlebars = Handlebars || {};",
-    "Handlebars.templates = Handlebars.templates || {} ;",
-    "var template = OriginalHandlebars.compile(" + JSON.stringify(contents) + ");",
-    "Handlebars.templates[" + JSON.stringify(templateName) + "] = function (data, partials) { ",
-    "partials = (partials || {});",
-    "return template(data || {}, { ",
-      "helpers: OriginalHandlebars.helpers,",
-      "partials: partials,",
-      "name: " + JSON.stringify(templateName),
-     "});",
-    "};"
-  ].join('');
-  
-  compileStep.addJavaScript({
-    path: path.join(path_part, 'handlebars.' + basename + '.js'),
-    sourcePath: compileStep.inputPath,
-    data: js
-  });
-});
+    file.addJavaScript({
+      data: output,
+      path: `${file.getPathInPackage()}.js`,
+    });
+  }
+
+  getCacheKey(file){
+    return [ file.getSourceHash() ];
+  }
+
+  compileHandlebar(file){
+    const templateName = file.getBasename().replace(/(.hbs)|(.handlebars)/, '');
+
+    const content = EJSON.stringify(file.getContentsAsString());
+    const output =`
+      Handlebars.templates = Handlebars.templates || {};
+      var template = OriginalHandlebars.compile('${content}');
+      Handlebars.templates['${templateName}'] = function(data, partials){
+        partials = partials || {};
+        return template(data || {}, {
+          helpers: OriginalHandlebars.helpers,
+          partials: partials,
+          name: '${templateName}'
+         });
+      }`;
+
+    return output;
+  }
+}
