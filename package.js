75bd80ffdcfa7fb2c53d198fdfe0bf6ba9f383ca
Gabriel Rubens <grsabreu@gmail.com>
2016-08-04 14:49:08 -0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'astrocoders:handlebars-server',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'Allows handlebars templates to be defined on the server in .handlebars files',
   git: 'https://github.com/Astrocoders/meteor-handlebars-server',
   documentation: 'README.md',
